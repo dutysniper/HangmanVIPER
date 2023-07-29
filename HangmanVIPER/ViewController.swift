d74fd6ac0f876f976674c86c6794eaf633f0caec
dutysniper <natarov.constantin@yandex.ru>
2023-07-29 17:06:46 +0300

    Initial commit

---
+++ HangmanVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HangmanVIPER
+//
+//  Created by Константин Натаров on 29.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
