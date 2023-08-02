08f185d2556561ffc259cdf33bc02f43e6a2103a
dutysniper <natarov.constantin@yandex.ru>
2023-08-02 18:07:56 +0300

    mokTest

--- HangmanVIPER/Modules/ModeSelectionModule/View/ModeSelectionView.swift
+++ HangmanVIPER/Modules/ModeSelectionModule/View/ModeSelectionView.swift
@@ -17,12 +17,26 @@ protocol ModeSelectionViewOutput {
 }
 
 final class ModeSelectionView: UIViewController, ModeSelectionViewInput {
-    let mokEntity: ModeSelectionButtonEntity
-    let button = ButtonFactory().createButton(with: <#T##ButtonEntityProtocol#>)
+    let buttonFactory = ButtonFactory()
+    let mokEntity = ModeSelectionButtonEntity(
+        title: "Hello",
+        titleColor: .black,
+        backgroundColor: .gray
+    )
+    private let containerView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(containerView)
+        let button = buttonFactory.createButton(with: mokEntity)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor)]
+        )
+        containerView.addSubview(button)
 //        setupNavigationBar()
     }
 }
