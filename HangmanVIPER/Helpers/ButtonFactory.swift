11183b6bbec487f3024a85a09ba1263bd85dda67
dutysniper <natarov.constantin@yandex.ru>
2023-08-07 23:47:08 +0300

    //

--- HangmanVIPER/Helpers/ButtonFactory.swift
+++ HangmanVIPER/Helpers/ButtonFactory.swift
@@ -19,6 +19,12 @@ final class ButtonFactory {
         button.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         button.widthAnchor.constraint(equalToConstant: 150).isActive = true
         button.widthAnchor.constraint(lessThanOrEqualToConstant: 400).isActive = true
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = false // Разрешим тень выходить за пределы кнопки
+        button.layer.shadowColor = UIColor.gray.cgColor
+        button.layer.shadowOpacity = 0.8
+        button.layer.shadowOffset = CGSize(width: 0, height: 4)
+        button.layer.shadowRadius = 6
         return button
     }
 }
--- HangmanVIPER/Modules/ModeSelectionModule/View/ModeSelectionView.swift
+++ HangmanVIPER/Modules/ModeSelectionModule/View/ModeSelectionView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 protocol ModeSelectionViewInput: AnyObject {
 
@@ -16,57 +17,65 @@ protocol ModeSelectionViewOutput {
     func userSelectedMode()
 }
 
-final class ModeSelectionView: UIViewController, ModeSelectionViewInput {
+
+class ModeSelectionView: UIViewController, ModeSelectionViewInput {
     let buttonFactory = ButtonFactory()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
+
         let redButtonEntity = ModeSelectionButtonEntity(
             title: "Одиночная игра",
-            titleColor: .black,
-            backgroundColor: .yellow
+            titleColor: .white,
+            backgroundColor: UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1.0)
         )
+
         let blueButtonEntity = ModeSelectionButtonEntity(
             title: "Мультиплеер",
-            titleColor: .black,
-            backgroundColor: .yellow
+            titleColor: .white,
+            backgroundColor: UIColor(red: 0/255, green: 0/255, blue: 255/255, alpha: 1.0)
         )
+
         let redButton = buttonFactory.createButton(with: redButtonEntity)
         let blueButton = buttonFactory.createButton(with: blueButtonEntity)
-        
+
         let buttonsStackView = UIStackView(arrangedSubviews: [redButton, blueButton])
         buttonsStackView.axis = .vertical
         buttonsStackView.alignment = .center
         buttonsStackView.spacing = 20
-        
+
         view.addSubview(buttonsStackView)
-        
+
         buttonsStackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate(
-            [
-                buttonsStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-                buttonsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ]
-        )
-//        setupNavigationBar()
+        NSLayoutConstraint.activate([
+            buttonsStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            buttonsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 }
 
-//MARK: - SetupUI
-private extension ModeSelectionView {
-//    func setupNavigationBar() {
-//        title = "Режим игры"
-//        navigationController?.navigationBar.prefersLargeTitles = true
-//
-//        let navBarAppearance = UINavigationBarAppearance()
-//        navBarAppearance.backgroundColor = UIColor(
-//            red: 21/255,
-//            green: 101/255,
-//            blue: 192/255,
-//            alpha: 194/255
-//        )
-//
-//        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
-//    }
+//MARK: - SwiftUI Preview
+@available(iOS 13.0, *)
+struct ModeSelectionViewPreview: PreviewProvider {
+    static var previews: some View {
+        // Оборачиваем UIHostingController в SwiftUI представление
+        ViewControllerWrapper()
+            .edgesIgnoringSafeArea(.all)
+            .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+            .previewDisplayName("Mode Selection View")
+    }
 }
+
+@available(iOS 13.0, *)
+struct ViewControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ViewControllerWrapper>) -> ModeSelectionView {
+        let modeSelectionView = ModeSelectionView()
+        return modeSelectionView
+    }
+
+    func updateUIViewController(_ uiViewController: ModeSelectionView, context: UIViewControllerRepresentableContext<ViewControllerWrapper>) {
+        
+    }
+}
+
