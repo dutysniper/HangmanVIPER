004a6a684463308baeb240ebce5002387cc66f77
dutysniper <natarov.constantin@yandex.ru>
2023-08-15 14:06:13 +0300

    refactoring randomWord method
    make it universal
    + display mock data in green Screen

--- HangmanVIPER/Modules/SingleGameMainModule/SingleGameMainScreenViewController.swift
+++ HangmanVIPER/Modules/SingleGameMainModule/SingleGameMainScreenViewController.swift
@@ -12,17 +12,48 @@ final class SingleGameMainScreenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
-        testMokEntity()
+//        testMokEntity()
+        testMockEntity()
+        
     }
 }
 
-func testMokEntity() {
-    let networkManager = NetworkManager.shared
-    networkManager.getRandomWord(fileName: "wordListWithDefinition") { word in
-        if let randomWord = word {
-            print("Random word: \(randomWord.word)")
-        } else {
-            print("No random word available.")
+
+//MARK: - Setup MOK UI
+extension SingleGameMainScreenViewController {
+    private func setupUI(with word: WordModel) {
+        let wordLabel = UILabel()
+        let wordDefinition = UILabel()
+        let labelsStackView = UIStackView(arrangedSubviews: [wordLabel, wordDefinition])
+        
+        wordLabel.text = word.word
+        wordLabel.font = UIFont.systemFont(ofSize: 30)
+        wordDefinition.text = word.entry.definition
+        wordDefinition.numberOfLines = 0
+        
+        labelsStackView.axis = .vertical
+        labelsStackView.alignment = .center
+        labelsStackView.spacing = 20
+        
+        view.addSubview(labelsStackView)
+        
+        labelsStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            labelsStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            labelsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            labelsStackView.widthAnchor.constraint(equalToConstant: 350)
+        ])
+    }
+   private func testMockEntity() {
+        let networkManager = NetworkManager.shared
+        networkManager.fetchRandomWord(fileName: "wordListWithDefinition") { result in
+            switch result {
+            case .success(let word):
+                print(word.word)
+                self.setupUI(with: word)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
         }
     }
 }
