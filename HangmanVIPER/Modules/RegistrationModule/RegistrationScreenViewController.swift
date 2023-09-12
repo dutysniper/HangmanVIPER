cb0c3ea488999d6131aa9071b0b1656ad71d7aab
dutysniper <natarov.constantin@yandex.ru>
2023-09-12 16:45:24 +0300

    //

---
+++ HangmanVIPER/Modules/RegistrationModule/RegistrationScreenViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrationScreenViewController.swift
+//  HangmanVIPER
+//
+//  Created by Константин Натаров on 11.09.2023.
+//
+
+import UIKit
+
+final class RegistrationScreenViewController: UIViewController {
+    
+    private let firstNameTF = UITextField()
+    private let lastNameTF = UITextField()
+    private let loginTF = UITextField()
+    private let passwordTF = UITextField()
+    private let emailTF = UITextField()
+    private let repeatedEmailTF = UITextField()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
